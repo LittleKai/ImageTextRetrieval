7d40ea620b9aabd9b2685a449de078197560e90d
LittleKai <aduc5525@gmail.com>
2024-08-05 16:35:29 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/littlekai/video_search_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.littlekai.video_search_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+// import 'home_page.dart';
+// import 'home_page_sketch.dart';
+// import 'home_page_colab.dart';
+import 'home_page_UI.dart';
+// import 'test_home_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Image Search App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
